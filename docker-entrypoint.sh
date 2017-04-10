830d77a4cf9f66a3f389c993b895381183a60c53
JerryHopper <hopper.jerry@gmail.com>
2017-04-10 22:41:34 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
+
+if [[ "$1" = "mitmdump" ]]; then
+        mkdir -p "$MITMPROXY_PATH"
+        chown -R mitmproxy:mitmproxy "$MITMPROXY_PATH"
+
+        su-exec mitmdump -s /usr/local/bin/app/ingress-mitm.py
+        # "$@"
+else
+        exec mitmdump -s /usr/local/bin/app/ingress-mitm.py
+        #"$@"
+fi
